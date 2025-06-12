b5f3995f8fe362258cbd89eaa3635c15f1e43fad
ReyesSalazarJohn <u21319856@utp.edu.pe>
2025-06-12 00:00:19 -0500

    feat:CursePage

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -1,14 +1,32 @@
 import useAuth from '../hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
 import CourseTable from '../components/CourseTable';
+import EvaluationManager from '../components/EvaluationManager';
+import Tabs from '../components/UI/Tabs.jsx'; // Asegúrate de importar el Tabs que creaste
+import ScoreManager from '../components/ScoreManager.jsx';
 
 const CoursePage = () => {
   const { user, person, logout } = useAuth();
   const navigate = useNavigate();
 
+  const tabData = [
+    {
+      label: "Cursos",
+      content: <CourseTable />
+    },
+    {
+      label: "Evaluaciones",
+      content: <EvaluationManager />
+    },
+    {
+      label: "Notas",
+      content: <ScoreManager />
+    }
+  ];
+
   return (
-    <div>
-      <CourseTable />
+    <div className="min-h-screen">
+      <Tabs tabs={tabData} />
     </div>
   );
 };
